c70735a7c469a792f4088e2ddec605917065e352
Laura Berge <lauraeberge@gmail.com>
2020-01-20 14:53:00 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      state[index].votes += 1
+      return state;
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      if (state[index].votes > 0) {
+        state[index].votes -= 1
+      }
+      return state;
+
+    default:
+      return state;
+  }
 }
